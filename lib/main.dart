cd1df81ab52700bb526710ce11d98228c1694a1b
tshabalalaaj <tshabalalaaj@gmail.com>
2024-05-22 10:06:02 +0000

    final

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,22 @@ class XylophoneApp extends StatelessWidget {
     player.play(AssetSource('note$soundNumber.wav'));
   }
 
+Expanded buildkey({required int soundNumber, required Color color}){
+    return Expanded(
+      child: TextButton( 
+        onPressed: (){
+          playSound(soundNumber);
+          },
+          style:TextButton.styleFrom(
+            backgroundColor: color,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(.0),),
+          ),
+        child: const Text(''),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,104 +38,13 @@ class XylophoneApp extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              
-              Expanded(
-                child: TextButton( 
-                    onPressed: (){
-                      playSound(1);
-                    }, 
-                    style:TextButton.styleFrom(
-                    backgroundColor: Colors.red,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(.0),),
-                  ),
-                    child: const Text(''),
-                  ),
-              ),
-
-              Expanded(
-                child: TextButton( 
-                  onPressed: (){
-                    playSound(2);
-                  }, 
-                  style: TextButton.styleFrom(
-                    backgroundColor: Colors.orange,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(.0),),
-                  ), 
-                  child: const Text(' '),
-                ),
-              ),
-
-              Expanded(
-                child: TextButton( 
-                  onPressed: (){
-                    playSound(3);
-                  }, 
-                  style: TextButton.styleFrom(
-                    backgroundColor: Colors.yellow,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(.0),),
-                  ), 
-                  child: const Text(' '),
-                ),
-              ),
-
-              Expanded(
-                child: TextButton( 
-                  onPressed: (){
-                    playSound(4);
-                  }, 
-                  style: TextButton.styleFrom(
-                    backgroundColor: Colors.green,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(.0),),
-                  ), 
-                  child: const Text(' '),
-                ),
-              ),
-
-              Expanded(
-                child: TextButton( 
-                  onPressed: (){
-                    playSound(5);
-                  }, 
-                  style: TextButton.styleFrom(
-                    backgroundColor: Colors.cyan,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(.0),),
-                  ), 
-                  child: const Text(' '),
-                ),
-              ),
-
-              Expanded(
-                child: TextButton( 
-                  onPressed: (){
-                    playSound(6);
-                  }, 
-                  style: TextButton.styleFrom(
-                    backgroundColor: Colors.blue,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(.0),),
-                  ), 
-                  child: const Text(' '),
-                ),
-              ),
-
-              Expanded(
-                child: TextButton( 
-                  onPressed: (){
-                    playSound(7);
-                  }, 
-                  style: TextButton.styleFrom(
-                    backgroundColor: Colors.purple,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(.0),),
-                  ), 
-                  child: const Text(' '),
-                ),
-              ),
+              buildkey(soundNumber: 1, color: Colors.yellow),
+              buildkey(soundNumber: 2, color: Colors.red),
+              buildkey(soundNumber: 3, color: Colors.green),
+              buildkey(soundNumber: 4, color: Colors.blue),
+              buildkey(soundNumber: 5, color: Colors.purple),
+              buildkey(soundNumber: 6, color: Colors.orange),
+              buildkey(soundNumber: 7, color: Colors.pink),
             ],
           ),
       )
